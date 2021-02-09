fea3884d20ec431361d2607decdf4b979d42496e
Steve Bjorg <steve.bjorg@gmail.com>
2021-02-09 12:56:09 -0800

    fix test script

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -25,7 +25,7 @@ if [ -z "$1" ]; then
     fi
 
     rm $LAMBDASHARP/Tests/Modules/Results/*.json > /dev/null 2>&1
-    dotnet $LAMBDASHARP/src/LambdaSharp.Tool/bin/Debug/netcoreapp3.1/LambdaSharp.Tool.dll deploy \
+    dotnet $LAMBDASHARP/src/LambdaSharp.Tool/bin/Debug/net5.0/LambdaSharp.Tool.dll deploy \
         --verbose:exceptions \
         --no-beep \
         --tier Test \
